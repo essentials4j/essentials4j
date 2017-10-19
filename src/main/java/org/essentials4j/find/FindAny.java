7f0037e5d3b0efb1d1dd62ba3c7818f6d14e3c93
Nikolche Mihajlovski <nikolce.mihajlovski@gmail.com>
2017-10-19 19:50:20 +0200

    Initial commit: FindAny.java

---
+++ src/main/java/org/essentials4j/find/FindAny.java
@@ -0,0 +1,43 @@
+package org.essentials4j.find;
+
+/*
+ * #%L
+ * essentials4j
+ * %%
+ * Copyright (C) 2017 Nikolche Mihajlovski
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+/**
+ * @author Nikolche Mihajlovski
+ * @since 1.0.0
+ */
+public class FindAny<T> {
+
+	private final Stream<T> stream;
+
+	public FindAny(Stream<T> stream) {
+		this.stream = stream;
+	}
+
+	public Optional<T> where(Predicate<? super T> predicate) {
+		return stream.filter(predicate).findAny();
+	}
+
+}
